cb460578d2e9c08d66dc59b64db64de8cae368b9
d.vilnikov <d.vilnikov@gazintech.ru>
2020-01-28 16:33:57 +0300

    chapter two is finished (Spring JPA)

--- src/main/java/ru/dvilnikov/springinaction/shavacloud/ShavaCloudApplication.java
+++ src/main/java/ru/dvilnikov/springinaction/shavacloud/ShavaCloudApplication.java
@@ -1,13 +1,41 @@
 package ru.dvilnikov.springinaction.shavacloud;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import ru.dvilnikov.springinaction.shavacloud.data.IngredientRepository;
 
 @SpringBootApplication
 public class ShavaCloudApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ShavaCloudApplication.class, args);
+    }
+    
+    @Bean
+    public CommandLineRunner dataLoader(IngredientRepository ingredientRepository) {
+        return new CommandLineRunner() {
+            @Override
+            public void run(String... args) throws Exception {
+                ingredientRepository.save(new Ingredient("FLTO", "Пшеничная Тортилья", Ingredient.Type.WRAP));
+                ingredientRepository.save(new Ingredient("COTO", "Кукурузная Тортилья", Ingredient.Type.WRAP));
+                ingredientRepository.save(new Ingredient("FLPT", "Пшеничный Лаваш", Ingredient.Type.WRAP));
+                ingredientRepository.save(new Ingredient("CHPT", "Сырный Лаваш", Ingredient.Type.WRAP));
+                ingredientRepository.save(new Ingredient("GRBF", "Говядина", Ingredient.Type.PROTEIN));
+                ingredientRepository.save(new Ingredient("PORK", "Свинина", Ingredient.Type.PROTEIN));
+                ingredientRepository.save(new Ingredient("CHCK", "Курятина", Ingredient.Type.PROTEIN));
+                ingredientRepository.save(new Ingredient("TMTO", "Помидорки", Ingredient.Type.VEGGIES));
+                ingredientRepository.save(new Ingredient("CBBG", "Капуста", Ingredient.Type.VEGGIES));
+                ingredientRepository.save(new Ingredient("ONIO", "Лук", Ingredient.Type.VEGGIES));
+                ingredientRepository.save(new Ingredient("PCKL", "Огурцы", Ingredient.Type.VEGGIES));
+                ingredientRepository.save(new Ingredient("CHED", "Чеддар", Ingredient.Type.CHEESE));
+                ingredientRepository.save(new Ingredient("JACK", "Монтери-Джек", Ingredient.Type.CHEESE));
+                ingredientRepository.save(new Ingredient("SLSA", "Сальса", Ingredient.Type.SAUCE));
+                ingredientRepository.save(new Ingredient("SRCR", "Сырный", Ingredient.Type.SAUCE));
+                ingredientRepository.save(new Ingredient("GRLC", "Чесночный", Ingredient.Type.SAUCE));
+            }
+        };
     }
 
 }
